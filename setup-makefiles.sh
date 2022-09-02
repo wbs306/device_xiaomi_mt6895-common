439269dfcbf40ea564782eee9a712cf7e7d2cf47
wbs306 <wbs306@gmail.com>
2022-09-02 22:09:17 +0800

    mt6895-common: Add devices to guard

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers
+write_headers "xaga rubens"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
