30ecf48d0f02c4ce066fd8df818da3fdd037cafe
wbs306 <wbs306@gmail.com>
2023-01-07 16:49:59 +0800

    mt6895-common: Build more ndk_platform libraries

--- mt6895-common.mk
+++ mt6895-common.mk
@@ -211,11 +211,14 @@ PRODUCT_COPY_FILES += \
 
 # NDK Platform backend
 PRODUCT_PACKAGES += \
+    android.hardware.gnss-V1-ndk_platform.vendor:64 \
     android.hardware.memtrack-V1-ndk_platform.vendor:64 \
+    android.hardware.power-V2-ndk_platform.vendor:64 \
     android.hardware.security.keymint-V1-ndk_platform.vendor:64 \
     android.hardware.security.sharedsecret-V1-ndk_platform.vendor:64 \
     android.hardware.security.secureclock-V1-ndk_platform.vendor:64 \
-    android.hardware.vibrator-V1-ndk_platform.vendor:64
+    android.hardware.vibrator-V1-ndk_platform.vendor:64 \
+    android.system.keystore2-V1-ndk_platform.vendor:64
 
 # NFC
 PRODUCT_PACKAGES += \
