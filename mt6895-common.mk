546efd9191f06c96713a33b34f18f8d16763cea0
wbs306 <wbs306@gmail.com>
2023-01-08 16:53:08 +0800

    mt6895-common: Move miuicamera permission file to product

--- mt6895-common.mk
+++ mt6895-common.mk
@@ -196,7 +196,7 @@ PRODUCT_PACKAGES += \
 
 # MIUI Camera
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miuicamera.xml
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-miuicamera.xml
 
 # MtkInCallService
 PRODUCT_PACKAGES += \
