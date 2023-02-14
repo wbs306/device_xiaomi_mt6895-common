7c5afee0873ea886acf38eded46ab05f14432ac7
Pranav Vashi <neobuddy89@gmail.com>
2023-02-14 22:49:58 +0800

    mt6895-common: sepolicy: Import Dolby dms related sepolicy from sm8150-common

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -155,6 +155,7 @@ VENDOR_SECURITY_PATCH := 2022-12-01
 
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot
