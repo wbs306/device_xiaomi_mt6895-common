40a5b16723d90aa89f34a650a46aa8966337ff5f
wbs306 <wbs306@gmail.com>
2023-07-15 21:06:00 +0800

    mt6895-common: Update vendor security patch level

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -163,7 +163,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-12-01
+VENDOR_SECURITY_PATCH := 2023-06-01
 
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
